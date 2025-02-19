a9fb80ef95dc2ba6ca4d595c9860d4d2e36d16ac
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-19 15:23:07 +0300

    Addedd the staticResources

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -2,10 +2,14 @@ package com.example
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.routing.get
+
 fun Application.configureRouting() {
     routing {
+        staticResources("/content","mycontent")
         get("/") {
             call.respondText("Hello World!")
         }
