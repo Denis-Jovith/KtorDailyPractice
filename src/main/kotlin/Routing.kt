8da2d2f431e9525bdf3c602931a1dc3f211f30e3
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-19 17:46:47 +0300

    wrote testing codes

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -3,23 +3,31 @@ package com.example
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.routing.get
 
 fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<IllegalStateException> { call, cause ->
+            call.respond(HttpStatusCode.InternalServerError, "App in illegal state: ${cause.message}")
+        }
+    }
     routing {
-        staticResources("/content","mycontent")
+        staticResources("/content", "mycontent")
+
         get("/") {
             call.respondText("Hello World!")
         }
 
-        get("/test1") {
-            val text = "<h1>Hello From Tanzania Ktor</h1>"
-            val type = ContentType.parse("text/html")
-            call.respondText(text, type)
+        get("/error-test") {
+            throw IllegalStateException("Too Busy")
         }
 
+        get("/test1") {
+            val text = "<h1>Hello From Tanzania Ktor</h1>"
+            val type = ContentType.Text.Html
+            call.respondText(text, type)
+        }
     }
-
 }
