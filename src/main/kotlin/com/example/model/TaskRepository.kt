a0fbe27550ac1594a0219e7e88c56a47c254dffd
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-20 14:04:28 +0300

    wrote the mutableListOf to store the Tasks

--- src/main/kotlin/com/example/model/TaskRepository.kt
+++ src/main/kotlin/com/example/model/TaskRepository.kt
@@ -1,2 +1,8 @@
 package com.example.com.example.model
 
+val task = mutableListOf(
+    Task("cleaning","Clean the house", Priority.Low),
+    Task("garden","Mow the lawn",Priority.Medium),
+    Task("shopping","Buy the groceries",Priority.High),
+    Task("panting","Painting the fence",Priority.Medium)
+)
