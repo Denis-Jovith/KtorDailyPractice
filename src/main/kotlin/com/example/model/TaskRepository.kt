4f79c2592dff5e8f1bfdb5fc320e823681c75f9c
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-20 14:29:58 +0300

    Refactoring the repository by encapsulationg the lists of values within a repository

--- src/main/kotlin/com/example/model/TaskRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.com.example.model
-
-val task = mutableListOf(
-    Task("cleaning","Clean the house", Priority.Low),
-    Task("garden","Mow the lawn",Priority.Medium),
-    Task("shopping","Buy the groceries",Priority.High),
-    Task("panting","Painting the fence",Priority.Medium)
-)
