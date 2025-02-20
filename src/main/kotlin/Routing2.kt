5c80025500aa3eab78ba8a6d6fcdaa22ff93be55
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-20 13:21:23 +0300

    wrote some html static codes

--- src/main/kotlin/Routing2.kt
+++ src/main/kotlin/Routing2.kt
@@ -1,6 +1,28 @@
 package com.example
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 
-fun Application.configureRouting2(){}
+fun Application.configureRouting1(){
+    routing{
+        get("/task"){
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = """
+                    <h3> TODO: </h3>
+                    <ol>
+                        <li>A table of all the tasks</li>
+                        <li>A form to submit new tasks</li>
+                    </ol>
+                """.trimIndent()
+
+            )
+
+        }
+    }
+}
