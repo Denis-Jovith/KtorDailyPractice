bedc5ec045893c07b4863dca35ba64ce3c853322
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-20 14:31:17 +0300

    mad the TaskRepository object

---
+++ src/main/kotlin/com/example/model/TaskRepository.kt
@@ -0,0 +1,11 @@
+package com.example.com.example.model
+
+object TaskRepository{
+    private val tasks = mutableListOf(
+        Task("cleaning", "Clean the house", Priority.Low),
+        Task("gardening", "Mow the lawn", Priority.Medium),
+        Task("shopping", "Buy the groceries", Priority.High),
+        Task("painting", "Paint the fence", Priority.Medium)
+    )
+
+}
