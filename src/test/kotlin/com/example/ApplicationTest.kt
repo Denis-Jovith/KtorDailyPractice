927f3e5a43b8325ce57fd0ff12aa8f6c652904a7
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-21 13:23:43 +0300

    Starting to write the Unit Tests

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,24 +1,5 @@
 package com.example
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.server.testing.*
-import junit.framework.TestCase.assertEquals
-import org.junit.Test
+class ApplicationTest{
 
-class ApplicationTest {
-
-    @Test
-    fun testNewEndpoint() = testApplication {
-        application {
-            module()
-        }
-
-        val response = client.get("/test1")
-
-        assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals("html", response.contentType()?.contentSubtype)
-        assertEquals("<h1>Hello From Tanzania Ktor</h1>", response.bodyAsText().trim())
-    }
 }
