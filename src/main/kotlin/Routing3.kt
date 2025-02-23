2e1e3e486fdf5caad3ab9bab68600be8624a9439
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-23 12:31:52 +0300

    deleting the Routing3.kt

--- src/main/kotlin/Routing3.kt
+++ src/main/kotlin/Routing3.kt
@@ -1,2 +1,11 @@
 package com.example
 
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
+
+
+fun Application.configureRouting3(){
+    routing {
+    }
+}
