aa22d4f63050a61d115ef21d2a37ad6d83ca24e7
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-25 22:13:20 +0300

    made some changes reviewing code

--- src/main/kotlin/Routing2.kt
+++ src/main/kotlin/Routing2.kt
@@ -37,7 +37,7 @@ fun Application.configureRouting1(){
                 call.respond(HttpStatusCode.BadRequest)
                 return@get
             }
-            
+
             try {
                 val priority = Priority.valueOf(priorityAsText)
                 val tasks = TaskRepository.tasksByPriority(priority)
